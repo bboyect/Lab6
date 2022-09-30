da3dd9e4e60af8d45771d4e991dc75a2300938f4
Your Name <you@example.com>
2022-09-30 18:55:51 +0800

    Start Dynamic with pseudo code

--- R/Lab6.R
+++ R/Lab6.R
@@ -15,11 +15,56 @@ knapsack_objects <-
 #==============Brute Force Function=======
 brute_force_knapsack <- function(x,W){
   
-  if(any(colnames(x) != c("v", "w"))){stop()} # Check if the names of each column of dataframe are correct
-  if(is.data.frame(x)==FALSE ){stop()}
-  if( any(x) < 0){stop()}
+  # if(any(colnames(x) != c("v", "w"))){stop()} # Check if the names of each column of dataframe are correct
+  # if(is.data.frame(x)==FALSE ){stop()}
+  # if( any(x) < 0){stop()}
+
+}
+#=============End Brute Force Function=======
+
+
+
+#=============Dynamic programming=======
+  
+  #=====pseudo codes======
+  # array m[0..n, 0..W];
+  # for j from 0 to W do:
+  #   m[0, j] := 0
+  # for i from 1 to n do:
+  #   m[i, 0] := 0
+  # 
+  # for i from 1 to n do:
+  #   for j from 0 to W do:
+  #   if w[i] > j then:
+  #   m[i, j] := m[i-1, j]
+  # else:
+  #   m[i, j] := max(m[i-1, j], m[i-1, j-w[i]] + v[i])
+  #=====End pseudo codes========
+
+knapsack_dynamic <- function(x, W){
+  
+  m <- matrix(NA,W+1,length(x[,1]+1))
+   
+    for(i in 1:length(x[,1]) ){
+      for(j in 0:W){
+        if(x$w[i] > j){m[i+1, j+1] <- m[i, j+1]}
+        
+        else{m[i+1, j+1] <- max(m[i, j+1], m[i, j+1-x$w[i]] + x$v[i])}
+        
+        
+      }
+        
+   return(m)     
+        
+    }
   
   
   
   
 }
+
+
+
+
+#=============End Dynamic programming=======
+
